7e6b7d644c2435f67478b55eadcf245b2d5331f7
Victor César <tinhocsar@hotmail.com>
2025-05-18 13:04:45 -0300

    Update ocorrencia_mensal.py

--- ocorrencia_mensal.py
+++ ocorrencia_mensal.py
@@ -12,7 +12,6 @@ arquivos = [
     ("15Nik8kNDKH-hpAHk26UROH9vxTGJXhBD", "focos_br_todos-sats_2023.csv"),
     ("1U38lVunPdtpcaQ8IVFQlN7LWLhdSPETx", "focos_br_todos-sats_2022.csv"),
     ("1CUwf5L3W7L2TtQwX55bzLfkhbYJq7f00", "focos_br_todos-sats_2021.csv"),
-
 ]
 
 # Loop para verificar e baixar apenas os arquivos que ainda não existem
@@ -33,7 +32,7 @@ def ocorrencias_por_mes(anos):
 
     for ano in anos:
         try:
-            df = pd.read_csv(f'data/focos_br_todos-sats_{ano}.csv',usecols=['data_pas'])
+            df = pd.read_csv(f'data/focos_br_todos-sats_{ano}.csv', usecols=['data_pas'])
             df['data_pas'] = pd.to_datetime(df['data_pas'], errors='coerce')
             df = df.dropna(subset=['data_pas'])  # remove datas inválidas
             df['mes'] = df['data_pas'].dt.month
@@ -43,14 +42,23 @@ def ocorrencias_por_mes(anos):
         except Exception as e:
             print(f"Erro em {ano}: {e}")
 
+    # Nomes dos meses em português
+    meses_nome = [
+        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ]
+
     # Criação do gráfico
     plt.figure(figsize=(12, 6))
-    plt.plot(list(ocorrencias_mensais.keys()), list(ocorrencias_mensais.values()), 
-             marker='o', color='coral')
+    plt.plot(
+        list(ocorrencias_mensais.keys()),
+        list(ocorrencias_mensais.values()),
+        marker='o', color='coral'
+    )
     plt.title('Ocorrências de Incêndios por Mês (Soma de Todos os Anos 2021 até 2024)')
     plt.xlabel('Mês')
     plt.ylabel('Número de Ocorrências')
-    plt.xticks(range(1, 13))
+    plt.xticks(range(1, 13), meses_nome, rotation=45)
     plt.grid(True)
     plt.tight_layout()
     plt.savefig('Ocorrencia_por_mes.pdf', format='pdf')
